e07ebf887d5f89ab5f53b172b9a2985e8007e5ab
Kevin <kevinlai76@gmail.com>
2019-05-29 01:56:34 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -42,7 +42,8 @@ end
   
 def shortened_tweet_truncator(tweet)
   if word_substituter(tweet).length > 140
-    word_substituter(tweet)[0..]
+    word_substituter(tweet)[0..137] + "..."
+    
 end
   
   
